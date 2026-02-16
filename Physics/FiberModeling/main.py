58919a73441397d3cd09defc49de60892935ea9d
Robert Wolle <31664927+RobWolle@users.noreply.github.com>
2026-02-16 15:07:25 +0000

    implemented graphing of fiber amplitude loss

--- Physics/FiberModeling/main.py
+++ Physics/FiberModeling/main.py
@@ -8,11 +8,13 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams['figure.dpi']=200 # setting the dpi of outputted graphs.
-from numba import njit
-from scipy import sparse
+#from numba import njit
+#from scipy import sparse
 
 
 """
+WAVE AMPLITUDE
+
 Light travels as a plane wave. It has a complex amplitude described by: 
     A(z) = A(0)e^ikz, with z being the propagation direction
     k = k_r + ik_i: the real and imaginary parts of the wavenumber
@@ -34,17 +36,34 @@ alpha = 1                       # absorption coefficient
 
 beta = 1                        # phase constant
 
-n_core = 1.44                   # index of refraction
-n_cladding = 1.45
+n_core = 1.45                   # index of refraction
+n_cladding = 1.44
 
 lambda_vacuum = 1.5*10**(-3)    # 1500 nm wavelength
 
 a = 10*10**(-6)                 # 10 um radius of fiber core
 
-NA = np.sqrt(n_core**2 - n_cladding**2)
+A_0 = 1                         # Amplitude maximum
 
-V = 2*np.pi()/lambda_vacuum*a*NA
+NA = np.sqrt(n_core**2 - n_cladding**2)     # Numerical Aperture
+
+V = 2*np.pi/lambda_vacuum*a*NA            # 
+
+nx = 101
+
+x_min = 0
+x_max = 2.2
 
 
+hx = (x_max - x_min)/(nx-1)
 
 
+x = np.linspace(x_min,x_max,nx)
+A = np.zeros(nx)
+
+for  i in range(nx):
+    A[i] = A_0*np.exp(-alpha/2*x[i])
+
+plt.plot(x,A)
+
+plt.savefig('/workspaces/Projects/Physics/FiberModeling/output.png')
