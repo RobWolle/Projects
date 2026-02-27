da3532f22a9ffed728547c5c2ec4884604818e4c
Robert Wolle <31664927+RobWolle@users.noreply.github.com>
2026-02-27 15:49:53 +0000

    added Boltzmann law and E at wavelength function

--- Physics/FiberModeling/emissivity.py
+++ Physics/FiberModeling/emissivity.py
@@ -12,15 +12,28 @@ plt.rcParams['figure.dpi']=200 # setting the dpi of outputted graphs.
 Constants and conversion functions
 """
 c = 299792458
+h = 6.62607015*10**-34  # Planck Constant in Si
+k_B = 1.380649*10**-23  # Boltzmann Constant in Si
 
 # k = 2pi/lambda, lamda changes in a material if f stays constant
 # f = v/lambda
 # v = c/n
 
-def get_lambda_n(f, n, c):
+def get_lambda_n(f, n):
     v = c/n
     lambda_n = v/f
     return lambda_n
+
+def get_BoltzmannLaw(wavelength,T):
+    # This function returns the radiance of a black body at a given wavelength and temperature
+    B = (2*h*c**2/wavelength**5)*1/(np.exp(h*c/(wavelength*k_B*T))-1)
+    return B
+
+# Intensity loss:
+def get_I_absorption(k_n, lambda_vac, x_i):  # This is a different k (extinction coeff)
+    alpha = 4*np.pi*k_n/lambda_vac
+    I_absorption = np.exp(-alpha*x_i)
+    return I_absorption
 
 """
 Material Constants
@@ -35,12 +48,6 @@ TiN_1um_n = 1.7547
 TiN_1um_k = 3.4512
 #TiN_1um_k = 0.4512
 
-"""
-Input Conditions
-"""
-Amplitude = 1               # Constant amplitude of intitial signal. 
-lambda_vacuum = 1*10**-6    # Desired vacuum wavelength of light.
-nx = 1001                    # Resolution for each domain.
 
 # Define regions of material by defining only:
 # [0] = their widths <m> (to then call them in order to fill the space)
@@ -67,53 +74,56 @@ for i in range (layers):
 
 
 
-# Intensity loss:
-def get_I_absorption(k_n, lambda_vac, x_i):  # This is a different k (extinction coeff)
-    alpha = 4*np.pi*k_n/lambda_vac
-    I_absorption = np.exp(-alpha*x_i)
-    return I_absorption
+"""
+Input Conditions
+"""
 
-E_0 = Amplitude
-f = c/lambda_vacuum
+Amplitude = 1               # Constant amplitude of intitial signal. 
+lambda_vacuum = 1*10**-6    # Desired vacuum wavelength of light.
+resolution = 1001                    # Resolution for each domain.
 
-x_all = np.zeros(1)
-E_all = np.zeros(1)
-I_all = np.zeros(1)
-x_starts = [0]
-phi_starts = [0]
-d=0
-for domain in Domains:
-    lambda_n = get_lambda_n(f, domain[1],c)
-    x_min = 0
-    x_max = domain[0]
-    x = np.linspace(x_min,x_max, nx)
 
-    hx = (x_max - x_min)/(nx-1)
-    x_starts.append(x_max+x_starts[d]) # sets the start of the next values of x in the x_all list
-    
-    x_all = np.append(x_all, x+x_starts[d])
-    
-    E = np.zeros(nx)
-    I = np.zeros(nx)
-    for  i in range(nx):
-        E_vac = E_0*np.cos(2*np.pi/lambda_n*x[i]+2*np.pi*phi_starts[d])         # E(x) = E_0.cos(kx) = sqrt(I_0)
-        E[i] = E_vac*np.sqrt(get_I_absorption(domain[2], lambda_vacuum, x[i]))  # I(x) = I_0.e^(-alpha.x)
-        I[i] = E[i]**2
-    
-    
-    E_all = np.append(E_all, E)
-    I_all = np.append(I_all, I)
-    E_0 = E_0*np.sqrt(get_I_absorption(domain[2], lambda_vacuum, x[nx-1]))
-    phi_starts.append(phi_starts[d]+x_max/lambda_n)
-    
-    print(x_starts[d])
-    print(E)
-    d=d+1
+def E_at_lambda(Domains, Amplitude, lambda_vacuum, resolution):
+    nx = resolution
+    E_0 = Amplitude
+    f = c/lambda_vacuum
 
-E_all = np.delete(E_all, 0)
-I_all = np.delete(I_all, 0)
-x_all = np.delete(x_all, 0)
+    x_all = np.zeros(1)
+    E_all = np.zeros(1)
+    I_all = np.zeros(1)
+    x_starts = [0]
+    phi_starts = [0]
+    d=0
+    for domain in Domains:
+        lambda_n = get_lambda_n(f, domain[1],c)
+        x_min = 0
+        x_max = domain[0]
+        x = np.linspace(x_min,x_max, nx)
 
+        hx = (x_max - x_min)/(nx-1)
+        x_starts.append(x_max+x_starts[d]) # sets the start of the next values of x in the x_all list
+        
+        x_all = np.append(x_all, x+x_starts[d])
+        
+        E = np.zeros(nx)
+        for  i in range(nx):
+            E_vac = E_0*np.cos(2*np.pi/lambda_n*x[i]+2*np.pi*phi_starts[d])         # E(x) = E_0.cos(kx) = sqrt(I_0)
+            E[i] = E_vac*np.sqrt(get_I_absorption(domain[2], lambda_vacuum, x[i]))  # I(x) = I_0.e^(-alpha.x)
+        
+        
+        E_all = np.append(E_all, E)
+        E_0 = E_0*np.sqrt(get_I_absorption(domain[2], lambda_vacuum, x[nx-1]))
+        phi_starts.append(phi_starts[d]+x_max/lambda_n)
+        
+        d=d+1
+
+    E_all = np.delete(E_all, 0)
+    I_all = E_all**2
+    x_all = np.delete(x_all, 0)
+    return E_all, I_all, x_all
+
+
+E_all, I_all, x_all = E_at_lambda(Domains, Amplitude, lambda_vacuum, resolution)
 
 plt.plot(x_all,E_all)
 
@@ -124,3 +134,9 @@ plt.plot(x_all,I_all)
 
 plt.savefig('/workspaces/Projects/Physics/FiberModeling/outputI.png')
 
+"""
+Can easily be extended to sum intensity over a variety of wavelengths. 
+Might need to make everything after the domain inputs into a function.
+Then, I can iterate over a linspace of wavelengths and add each resulting E_all, then displaying over all wavelengths.
+There probably needs to also be a way to modify the E_all intensity at a particular wavelength according to a distribution of lightsource wavelength-dependent intensity.
+"""
